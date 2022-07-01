4612fcb94295cbb5131aee982347067fbf432e43
Casper <draakie@protonmail.com>
2022-07-01 23:47:57 +0200

    comments and spelling

--- scrape.py
+++ scrape.py
@@ -1,22 +1,33 @@
+# ------------------------------------------------------------------------------
+# scrape.py
+#
+# Scrape an ergodox layout from the ZSA ergodox layout website.
+# Because the print button isn't good enough.
+# ------------------------------------------------------------------------------
 
+# Constants
 URL_PREFIX = 'https://configure.zsa.io/ergodox-ez/layouts/'
+URL_TEMPLATE = 'https://configure.zsa.io/ergodox-ez/layouts/{0}/latest'
+URL_EXAMPLE = URL_TEMPLATE.format('XXXXX')
 
+# Argument parsing and validation
 def main():
     import argparse
     import validators
 
     try:
-        parser = argparse.ArgumentParser(description='Scape ZSA ergodox layout')
-        parser.add_argument('url', help='The URL for your layout. Must begin with: \'{0}\''.format(URL_PREFIX))
+        parser = argparse.ArgumentParser(description='Scrape ZSA ergodox layout')
+        parser.add_argument('url', help='The URL for your layout. Must look like: \'{0}\'. Append /X to select layer.'.format(URL_EXAMPLE))
 
         args = parser.parse_args()
 
         if not validators.url(args.url):
             raise Exception('URL provided is malformed.')
 
-        if (len(args.url) < len(URL_PREFIX)
+        # TODO: Use regex to validate the full URL
+        if (len(args.url) < len(URL_EXAMPLE)
             or not args.url.startswith(URL_PREFIX)):
-            raise Exception('URL must begin with: \'{0}\''.format(URL_PREFIX))
+            raise Exception('URL must look like: \'{0}\''.format(URL_EXAMPLE))
 
         scrape(args.url)
 
@@ -26,6 +37,7 @@ def main():
     except Exception as ex:
         print('error: {0}'.format(ex))
 
+# Program
 def scrape(url):
     # from selenium import webdriver
     #
@@ -33,8 +45,6 @@ def scrape(url):
 
     raise Exception('Not implemented.')
 
-# ------------------------------------------------------------------------------
 # Entry point
-# ------------------------------------------------------------------------------
 if __name__ == '__main__':
     main()
