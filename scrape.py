8731435f0fbcec5aa2c6e5dbfa136141805b34f5
Casper <draakie@protonmail.com>
2022-07-03 16:34:31 +0200

    hide logo, hide none icons

--- scrape.py
+++ scrape.py
@@ -9,10 +9,10 @@
 URL_PREFIX = 'https://configure.zsa.io/ergodox-ez/layouts/'
 URL_TEMPLATE = 'https://configure.zsa.io/ergodox-ez/layouts/{0}/latest'
 URL_EXAMPLE = URL_TEMPLATE.format('XXXXX')
-PAGE_LOAD_TIMEOUT = 10
-FILENAME = 'layout.png'
+PAGE_LOAD_TIMEOUT = 10 # Seconds
+OUTPUT_FILENAME = 'layout.png'
 TARGET_ELEMENT = 'ergodox'
-EXTRA_HEIGHT = 20
+EXTRA_HEIGHT = 20 # Pixels
 
 # Argument parsing and validation
 def main():
@@ -23,6 +23,8 @@ def main():
         parser = argparse.ArgumentParser(description='Scrape ZSA ergodox layout')
         parser.add_argument('url', help='The URL for your layout. Must look like: \'{0}\'. Append /X to select layer.'.format(URL_EXAMPLE))
         parser.add_argument('--hide-logo', action='store_true', help='Hide the EZ logo.')
+        parser.add_argument('--hide-none-icon', action='store_true', help='Hide the \'none\' icon.')
+        parser.add_argument('--hide-mod-color', action='store_true', help='Hide the colored background on modifer keys.')
 
         args = parser.parse_args()
 
@@ -34,18 +36,23 @@ def main():
             or not args.url.startswith(URL_PREFIX)):
             raise Exception('URL must look like: \'{0}\''.format(URL_EXAMPLE))
 
-        scrape(args.url, args.hide_logo)
-
     except Exception as ex:
         print('Error: {0}'.format(ex))
 
+    scrape(args)
+
 # Program
-def scrape(url, hide_logo):
+def scrape(args):
     import os
     import time
     from selenium import webdriver
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support.ui import WebDriverWait
+
+    url = args.url
+    hide_logo = args.hide_logo
+    hide_none_icon = args.hide_none_icon
+    hide_mod_color = args.hide_mod_color
 
     try:
         # NOTE: Using this sad hack because the --width and --height arguments
@@ -70,54 +77,57 @@ def scrape(url, hide_logo):
         browser.execute_script('window.scrollTo(0, 0)')
         time.sleep(0.5)
 
-        # TODO: Modify CSS for cleaner image.
-
         if hide_logo:
-            logo = browser.find_element(By.CLASS_NAME, 'logo')
-            # browser.execute_script('arguments[0].style.visibility = \'hidden\';', logo)
-            # This change is reflected in the print output, but not in the saved image. Maddening.
-            browser.execute_script('arguments[0].style.border = \'2px solid red\';', logo)
+            logo = browser.find_element(By.XPATH, '//div[@class=\'ergodox\']/div[@class=\'logo\']')
+            browser.execute_script('arguments[0].style.visibility = \'hidden\';', logo)
             time.sleep(0.5)
-            print(logo.value_of_css_property('border'))
+
+        if hide_none_icon:
+            browser.execute_script('document.styleSheets[1].insertRule(".icon-none:before { content: none !important; border: 2px solid red; }")')
+            time.sleep(0.5)
+
+        if hide_mod_color:
+            # TODO: Implement.
+            time.sleep(0.5)
+
+        # input('press ENTER to continue')
 
         screenshot(browser, element)
-
-        # print(element.get_attribute('innerHTML'))
 
     finally:
         try:
             browser.close()
         except Exception as ex:
-            print('Error: {0}'.format(ex))
+            raise Exception from ex
 
 # Save image of element to disk
 def screenshot(browser, element):
+    import os
     from PIL import Image
 
     try:
-        location = element.location
-        size = element.size
+        temp_filename = 'temp.png'
 
-        browser.save_screenshot('full.png')
+        browser.save_screenshot(temp_filename)
 
-        x0 = (int)(location['x'])
-        y0 = (int)(location['y'])
-        x1 = (int)(x0 + size['width'])
-        # Extra height is needed because the visual element extends outside the
+        x0 = (int)(element.location['x'])
+        y0 = (int)(element.location['y'])
+        x1 = (int)(x0 + element.size['width'])
+        # Extra height is added because the visual element extends outside the
         # element's reported borders.
-        y1 = (int)(y0 + size['height'] + EXTRA_HEIGHT)
+        y1 = (int)(y0 + element.size['height'] + EXTRA_HEIGHT)
 
-        print('{0}, {1}, {2}, {3}'.format(x0, y0, x1, y1))
-
-        image = Image.open('full.png')
+        image = Image.open(temp_filename)
         image = image.crop((x0, y0, x1, y1))
-        image = image.save(FILENAME)
+        image = image.save(OUTPUT_FILENAME)
+
+        os.remove(temp_filename)
 
         # TODO: Include path.
-        print('Image saved to: {0}'.format(FILENAME))
+        print('Image saved to: {0}'.format(OUTPUT_FILENAME))
 
     except Exception as ex:
-        print('Error: {0}'.format(ex))
+        raise Exception from ex
 
 # Entry point
 if __name__ == '__main__':
